c83cdff42d458b273c1ecdb5ae1a0753650f8d0f
Gera Amaya <gerardoamaya.10@gmail.com>
2024-07-13 13:20:16 -0300

    stylos

--- js/script.js
+++ js/script.js
@@ -11,3 +11,26 @@ window.addEventListener('load', function () {
     }
   });
   
+
+  // Función para abrir el lightbox
+function openLightbox(img) {
+  var lightbox = document.getElementById('myLightbox');
+  var lightboxImg = document.getElementById('lightboxImg');
+  var caption = document.getElementById('caption');
+  lightbox.style.display = 'block';
+  lightboxImg.src = img.src;
+  caption.innerHTML = img.alt;
+}
+
+// Función para cerrar el lightbox
+function closeLightbox() {
+  var lightbox = document.getElementById('myLightbox');
+  lightbox.style.display = 'none';
+}
+
+// Añadir el evento onclick a cada imagen
+document.querySelectorAll('.fotoCabanas img').forEach(img => {
+  img.onclick = function() {
+    openLightbox(this);
+  };
+});
